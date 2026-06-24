57e0e161b542077673820ae800484418f1d10eb5
S Perera <sperera1922@gmail.com>
2026-06-24 22:01:30 +0530

    For loop

---
+++ For_loop.java
@@ -0,0 +1,9 @@
+public class For_loop {
+    public static void main(String []args){
+        int count = 0;
+        for(int i=0; i<5; i++){
+            count++;
+        }
+        System.out.println("The total count is " + count);
+    }
+}
